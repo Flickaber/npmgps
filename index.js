5d1db9834de031b8291d6fc05313738c0004a30f
LauraBernard <laura@Flickaber>
2017-05-17 17:01:37 +0200

    creation of the index.j and package.json

---
+++ index.js
@@ -0,0 +1,11 @@
+exports.trackEntersTheBox(gpsTrack,pointNorthWest,pointSouthEast){
+  console.log("toto");
+}
+
+exports.trackIsInTheBox(gpsTrack,pointNorthWest,pointSouthEast){
+
+}
+
+exports.getDistanceBetweenPointAndNearestGpsPoint(gpsTrack,gpsPoint){
+
+}
